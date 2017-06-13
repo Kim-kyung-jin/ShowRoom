a8a6276bc303bfefecf9c213b59e9f46aea29bda
parkjuhuck <gusehdcks3@gmail.com>
2017-06-13 14:24:43 +0900

    Create categoryRouter.js

--- showRoom/routes/categoryRouter.js
+++ showRoom/routes/categoryRouter.js
@@ -4,7 +4,4 @@ exports.setRequestUrl=function(app){
     app.post('/category', category.addCategory);  //카테고리 등록 (O)
     app.put('/category/:categoryCode', category.editCategory);  //카테고리 수정  (O)
     app.delete('/category/:categoryCode', category.deleteCategory);  //카테고리 삭제 (O)
-    app.post('/codyCategory/:categoryType', category.showCategoryTypeCody);  //선택한 카테고리(코디)보기 (O)
-    app.post('/codyCategoryDetail/:codyId', category.showCategoryCloth);  //카테고리 코디 상세보기   (O)
-   // app.post('/codySearch', category.codySearch);  //카테고리 코디 상세보기   (O)
 };
